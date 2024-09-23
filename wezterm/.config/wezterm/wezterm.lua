7ff074ddcc4eb8afb6bfa51d709bfa78f0f32ac0
Kade Allen <kadeallenprofessional@gmail.com>
2024-09-23 13:11:35 +1200

    adjust background in wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -16,7 +16,7 @@ config.enable_tab_bar = false
 config.adjust_window_size_when_changing_font_size = false
 
 config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.8
-config.macos_window_background_blur = 20
+config.window_background_opacity = 0.75
+config.macos_window_background_blur = 15
 
 return config
