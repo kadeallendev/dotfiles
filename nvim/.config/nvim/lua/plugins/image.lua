7472f7c856e7bb7086a87cae0b7d8e3a28a36020
Kade Allen <kadeallenprofessional@gmail.com>
2024-09-23 20:20:39 +1200

    disable image.nvim

--- nvim/.config/nvim/lua/plugins/image.lua
+++ nvim/.config/nvim/lua/plugins/image.lua
@@ -1,22 +1,22 @@
 return {
   {
     -- This doesn't work at the moment, duplicates the image and shit
-    '3rd/image.nvim',
-    event = 'VeryLazy',
-    dependencies = {
-      'vhyrro/luarocks.nvim',
-    },
-    config = function()
-      package.path = package.path .. ';' .. vim.fn.expand '$HOME' .. '/.luarocks/share/lua/5.1/?/init.lua'
-      package.path = package.path .. ';' .. vim.fn.expand '$HOME' .. '/.luarocks/share/lua/5.1/?.lua'
-      require('image').setup {
-        backend = 'ueberzug', -- Doesn't only shows outline of image
-        max_width = 100,
-        max_height_window_percentage = math.huge,
-        max_width_window_percentage = math.huge,
-        -- window_overlap_clear_enabled = true,
-        -- window_overlap_clear_ft_ignore = { 'cmp_menu', 'cmp_docs', '' },
-      }
-    end,
+    -- '3rd/image.nvim',
+    -- event = 'VeryLazy',
+    -- dependencies = {
+    --   'vhyrro/luarocks.nvim',
+    -- },
+    -- config = function()
+    --   package.path = package.path .. ';' .. vim.fn.expand '$HOME' .. '/.luarocks/share/lua/5.1/?/init.lua'
+    --   package.path = package.path .. ';' .. vim.fn.expand '$HOME' .. '/.luarocks/share/lua/5.1/?.lua'
+    --   require('image').setup {
+    --     backend = 'ueberzug', -- Doesn't only shows outline of image
+    --     max_width = 100,
+    --     max_height_window_percentage = math.huge,
+    --     max_width_window_percentage = math.huge,
+    --     -- window_overlap_clear_enabled = true,
+    --     -- window_overlap_clear_ft_ignore = { 'cmp_menu', 'cmp_docs', '' },
+    --   }
+    -- end,
   },
 }
