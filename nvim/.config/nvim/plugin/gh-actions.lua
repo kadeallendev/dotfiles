f7d9086932ee0bb5c86a6bfa093e9d791a599071
Kade Allen <kade.allen@ptml.com>
2024-11-19 09:14:44 +1300

    Add github actions autocmd

---
+++ nvim/.config/nvim/plugin/gh-actions.lua
@@ -0,0 +1,22 @@
+-- Start the GH actions language server
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'github-actions-workflow',
+  callback = function()
+    print 'IN GITHUB'
+    vim.lsp.start {
+      name = 'gh-actions-lsp',
+      filetypes = { 'github-actions-workflow' },
+      cmd = { 'npx', '@strozw/github-actions-languageserver', '--stdio' },
+      root_dir = vim.fn.getcwd(),
+      init_options = {},
+    }
+  end,
+})
+
+vim.cmd 'autocmd BufRead,BufNewFile *.github/workflows/*.yaml set filetype=github-actions-workflow'
+vim.cmd 'autocmd BufRead,BufNewFile *.github/workflows/*.yml set filetype=github-actions-workflow'
+vim.cmd 'autocmd BufRead,BufNewFile *.github/actions/*.yaml set filetype=github-actions-workflow'
+vim.cmd 'autocmd BufRead,BufNewFile *.github/actions/*.yml set filetype=github-actions-workflow'
+
+vim.treesitter.language.register('yaml', 'github-actions-workflow')
