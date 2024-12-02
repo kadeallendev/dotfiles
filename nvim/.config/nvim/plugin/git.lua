a67ef01e7652655831859804594b3e57e6435e78
Kade Allen <kade.allen@ptml.com>
2024-12-02 16:34:05 +1300

    Test with no prefix

--- nvim/.config/nvim/plugin/git.lua
+++ nvim/.config/nvim/plugin/git.lua
@@ -1,5 +1,8 @@
 -- Git related shit
 
+-- Neogit status
+vim.keymap.set('n', '<leader>gn', '<CMD>Neogit<CR>', { noremap = true, silent = true, desc = 'Neogit' })
+
 -- Hunk movement
 vim.keymap.set('n', ']h', '<CMD>Gitsigns next_hunk<CR>', { desc = 'Next hunk' })
 vim.keymap.set('n', '[h', '<CMD>Gitsigns prev_hunk<CR>', { desc = 'Previous hunk' })
@@ -7,22 +10,71 @@ vim.keymap.set('n', '[h', '<CMD>Gitsigns prev_hunk<CR>', { desc = 'Previous hunk
 -- Stage hunk
 vim.keymap.set('n', '<leader>gh', '<CMD>Gitsigns stage_hunk<CR>', { desc = 'Stage hunk' })
 
--- Stage current buffer and commit, prompting for message
-local function stage_and_commit()
-  -- Stage buffer
-  vim.cmd 'G stage %'
+-- Stage buffer
+vim.keymap.set('n', '<leader>gs', '<CMD>Gitsigns stage_buffer<CR>', { noremap = true, silent = true, desc = 'Stage buffer' })
 
-  -- Prompt for commit message
-  vim.ui.input({ prompt = 'Commit msg: ' }, function(input)
-    if input then
-      -- Create git commit
-      vim.cmd('G commit -m "' .. input .. '"')
-    else
+local jira_prefix_pat = '^[A-Za-z]+%-%d+'
+
+-- Commit with branch prefix
+local function get_branch_prefix()
+  local handle = io.popen 'git rev-parse --abbrev-ref HEAD 2> /dev/null'
+  if handle == nil then
+    print 'ERROR: unable to get branch name'
+    return
+  end
+  local branch_name = handle:read('*a'):gsub('%s+', '') -- Remove trailing whitespace
+  handle:close()
+  return branch_name:match(jira_prefix_pat)
+end
+
+-- Commit with the branch jira prefix prefixed to commit message
+local function commit()
+  local prompt = 'Commit msg: '
+  -- Get branch prefix
+  local branch_prefix = get_branch_prefix()
+  if branch_prefix ~= nil then
+    prompt = 'Commit msg [' .. branch_prefix .. ']: '
+  end
+
+  -- Prompt user
+  vim.ui.input({ prompt = prompt }, function(input)
+    -- Check if a message was provided
+    if not input or #input == 0 then
       print 'Commit message was not provided'
+      return
     end
+
+    -- If commit message contains another JIRA prefix, don't prepend branch prefix
+    if input:match(jira_prefix_pat) ~= nil then
+      vim.cmd('G commit -m "' .. input .. '"')
+      return
+    end
+
+    -- Commit with jira prefix
+    local msg = input
+    -- Prepend the branch prefix if it exists
+    if branch_prefix ~= nil then
+      msg = branch_prefix .. ' ' .. msg
+    end
+    vim.cmd('G commit -m "' .. msg .. '"')
   end)
 end
-vim.keymap.set('n', '<leader>gc', stage_and_commit, { noremap = true, silent = true, desc = 'Stage current file and commit' })
 
--- Push to upstream branch
-vim.keymap.set('n', '<leader>gp', '<CMD>G push origin<CR>', { noremap = true, silent = true, desc = 'Push to upstream branch' })
+vim.keymap.set('n', '<leader>gc', commit, { noremap = true, silent = true, desc = 'Commit with branch prefix' })
+
+-- -- Stage current buffer and commit, prompting for message
+-- local function stage_and_commit()
+--   -- Stage buffer
+--   vim.cmd 'G stage %'
+
+--   -- Prompt for commit message
+--   vim.ui.input({ prompt = 'Commit msg: ' }, function(input)
+--     if input then
+--       -- Create git commit
+--       vim.cmd('G commit -m "' .. input .. '"')
+--     else
+--       print 'Commit message was not provided'
+--     end
+--   end)
+-- end
+-- vim.keymap.set('n', '<leader>gc', stage_and_commit, { noremap = true, silent = true, desc = 'Stage current file and commit' })
