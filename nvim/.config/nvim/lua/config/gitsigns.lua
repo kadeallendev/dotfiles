ded131eb7706e699b15e4ecb396ea95efb11a8ee
Kade Allen <kade.allen@ptml.com>
2024-12-02 15:07:10 +1300

    Change to <leader>gc

--- nvim/.config/nvim/lua/config/gitsigns.lua
+++ nvim/.config/nvim/lua/config/gitsigns.lua
@@ -8,7 +8,6 @@ require('gitsigns').setup {
   -- },
 }
 
-
 -- Hunk movement
 vim.keymap.set('n', ']h', '<CMD>Gitsigns next_hunk<CR>', { desc = 'Next hunk' })
 vim.keymap.set('n', '[h', '<CMD>Gitsigns prev_hunk<CR>', { desc = 'Previous hunk' })
---
+++ nvim/.config/nvim/plugin/git.lua
@@ -0,0 +1,26 @@
+-- Git related shit
+
+-- Hunk movement
+vim.keymap.set('n', ']h', '<CMD>Gitsigns next_hunk<CR>', { desc = 'Next hunk' })
+vim.keymap.set('n', '[h', '<CMD>Gitsigns prev_hunk<CR>', { desc = 'Previous hunk' })
+
+-- Stage hunk
+vim.keymap.set('n', '<leader>gh', '<CMD>Gitsigns stage_hunk<CR>', { desc = 'Stage hunk' })
+
+-- Stage current buffer and commit, prompting for message
+local function stage_and_commit()
+  -- Stage buffer
+  require('gitsigns').stage_buffer()
+
+  -- Prompt for commit message
+  vim.ui.input({ prompt = 'Commit msg: ' }, function(input)
+    if input then
+      -- Create git commit
+      vim.cmd('G commit -m "' .. input .. '"')
+    else
+      print 'Commit message was not provided'
+    end
+  end)
+end
+
+vim.keymap.set('n', '<leader>gc', stage_and_commit, { noremap = true, silent = true, desc = 'Stage current file and commit' })
