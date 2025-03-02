c6f18bdc3c4c0f15a7a4c65ea754c7ccf9c85920
Kade Allen <kade.allen@ptml.com>
2025-03-03 11:05:33 +1300

    Disable plugins

--- nvim/.config/nvim/lua/plugins/vim-matchup.lua
+++ nvim/.config/nvim/lua/plugins/vim-matchup.lua
@@ -1,10 +1,10 @@
 -- Enables better use of '%' such as with if statements
 return {
-  {
-    'andymass/vim-matchup',
-    event = 'InsertEnter',
-    setup = function()
-      vim.g.matchup_matchparen_offscreen = { method = 'popup' }
-    end,
-  },
+  -- {
+  --   'andymass/vim-matchup',
+  --   event = 'InsertEnter',
+  --   setup = function()
+  --     vim.g.matchup_matchparen_offscreen = { method = 'popup' }
+  --   end,
+  -- },
 }
