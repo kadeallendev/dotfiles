733eba2c01a1892442a22b32aab05d6cf247d85d
Kade Allen <38875538+kadeallendev@users.noreply.github.com>
2025-05-21 16:28:39 +1200

    Add FugitiveChanged autocmd

--- nvim/.config/nvim/plugin/autocmds.lua
+++ nvim/.config/nvim/plugin/autocmds.lua
@@ -4,41 +4,59 @@ local utils = require 'utils'
 -- Inserts the current git branch into the 'b' register.
 -- Defaults to 'main' if not in git repo.
 --------------------------------------------------------
+local function set_reg_git_branch()
+  local branch = utils.get_git_branch()
+  if branch then
+    vim.fn.setreg('b', branch)
+  end
+end
+
 vim.api.nvim_create_autocmd('VimEnter', {
-  callback = function()
-    local branch = utils.get_git_branch()
-    if branch then
-      vim.fn.setreg('b', branch)
-    end
-  end,
+  callback = set_reg_git_branch,
+})
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'FugitiveChanged',
+  callback = set_reg_git_branch,
 })
 
 ----------------------------------------------
 -- Inserts the JIRA ticket to the 't' register
 ----------------------------------------------
-vim.api.nvim_create_autocmd('VimEnter', {
-  callback = function()
-    local ticket = utils.get_jira_ticket()
-    if ticket == nil then
-      return nil
-    end
+local function set_reg_jira_ticket()
+  local ticket = utils.get_jira_ticket()
+  if ticket == nil then
+    return nil
+  end
 
-    vim.fn.setreg('t', ticket)
-  end,
+  vim.fn.setreg('t', ticket)
+end
+
+vim.api.nvim_create_autocmd('VimEnter', {
+  callback = set_reg_jira_ticket,
+})
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'FugitiveChanged',
+  callback = set_reg_jira_ticket,
 })
 
 -----------------------------------------------------
 -- Inserts a link to the current branch's JIRA ticket
 -- into the 'j' register
 -----------------------------------------------------
-vim.api.nvim_create_autocmd('VimEnter', {
-  callback = function()
-    local ticket = utils.get_jira_ticket()
-    if ticket == nil then
-      return nil
-    end
+local function set_reg_jira_link()
+  local ticket = utils.get_jira_ticket()
+  if ticket == nil then
+    return nil
+  end
 
-    local jira_link = 'https://partstrader.atlassian.net/browse/' .. ticket
-    vim.fn.setreg('j', jira_link)
-  end,
+  local jira_link = 'https://partstrader.atlassian.net/browse/' .. ticket
+  vim.fn.setreg('j', jira_link)
+end
+
+vim.api.nvim_create_autocmd('VimEnter', {
+  callback = set_reg_jira_link,
+})
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'FugitiveChanged',
+  callback = set_reg_jira_link,
 })
