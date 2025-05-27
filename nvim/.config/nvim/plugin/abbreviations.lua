8c348bd886716896169df442a336ad3724259af3
Kade Allen <38875538+kadeallendev@users.noreply.github.com>
2025-05-27 16:38:10 +1200

    Add ISO and UTC times

--- nvim/.config/nvim/plugin/abbreviations.lua
+++ nvim/.config/nvim/plugin/abbreviations.lua
@@ -10,4 +10,6 @@ ab('i', '@@', 'kade.allen@ptml.com') -- Email
 ab('i', 'gb', vim.g.gitbranch) -- Git branch
 ab('i', 'ddate', os.date '%Y-%m-%d') -- Date
 ab('i', 'ttime', os.date '%H:%M:%S') -- Time
+ab('i', 'diso', vim.fn.system 'date -Iseconds') -- ISO 8601 time
+ab('i', 'udiso', vim.fn.system 'date -Iseconds --utc') -- UTC ISO 8601 time
 ab('i', 'td', 'TODO:') -- TODO
