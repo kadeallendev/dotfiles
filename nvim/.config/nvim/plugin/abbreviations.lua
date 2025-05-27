bed8126fe144d390fb49aad008177e35576fbf85
Kade Allen <38875538+kadeallendev@users.noreply.github.com>
2025-05-27 16:34:58 +1200

    Add abbreviations plugin with some cool shit

---
+++ nvim/.config/nvim/plugin/abbreviations.lua
@@ -0,0 +1,13 @@
+-- Abbreviations
+
+-- Helper to create abbreviations
+local function ab(mode, lhs, rhs)
+  vim.api.nvim_command(mode .. 'abbrev ' .. lhs .. ' ' .. rhs)
+end
+
+ab('i', 'PT', 'PartsTrader') -- PartsTrader
+ab('i', '@@', 'kade.allen@ptml.com') -- Email
+ab('i', 'gb', vim.g.gitbranch) -- Git branch
+ab('i', 'ddate', os.date '%Y-%m-%d') -- Date
+ab('i', 'ttime', os.date '%H:%M:%S') -- Time
+ab('i', 'td', 'TODO:') -- TODO
