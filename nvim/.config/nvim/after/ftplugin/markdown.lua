1a7e31d54edfdc19976c294c4b08d3684b49b358
Kade Allen <38875538+kadeallendev@users.noreply.github.com>
2025-06-09 09:37:50 +1200

    comment out some markdown config shit

--- nvim/.config/nvim/after/ftplugin/markdown.lua
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -1,7 +1,6 @@
--- vim.opt.conceallevel = 1
-vim.opt.conceallevel = 0
+-- vim.opt.conceallevel = 0
 vim.opt.spell = true
-vim.opt.foldmethod = 'manual'
+-- vim.opt.foldmethod = 'manual'
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 
