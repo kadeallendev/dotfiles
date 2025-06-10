66a865c9fcbbda101ef5476d320ae51f9a14ff41
Kade Allen <38875538+kadeallendev@users.noreply.github.com>
2025-06-11 11:36:36 +1200

    Delete symbols outline plugin

--- nvim/.config/nvim/lua/plugins/symbols-outline.lua
+++
@@ -1,9 +0,0 @@
--- Display symbols outline of file
-return {
-  'simrat39/symbols-outline.nvim',
-  cmd = 'SymbolsOutline',
-  opts = {},
-  config = function()
-    require('symbols-outline').setup()
-  end,
-}
