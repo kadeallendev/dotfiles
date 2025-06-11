67edef64f0fa90fce6e34db36a9f68e7896c8be2
Kade Allen <38875538+kadeallendev@users.noreply.github.com>
2025-06-11 14:39:43 +1200

    Update markdown preview to highlight checked lists and set conceallevel

--- nvim/.config/nvim/lua/kade/markdown-highlights.lua
+++ nvim/.config/nvim/lua/kade/markdown-highlights.lua
@@ -5,10 +5,10 @@ local M = {
 
 -- Define heading heading_colors
 local heading_colors = {
-  h1 = 'NvimLightBlue',
-  h2 = 'NvimLightRed',
-  h3 = 'NvimLightGreen',
-  h4 = 'NvimLightMagenta',
+  h1 = 'NvimLightMagenta',
+  h2 = 'NvimLightBlue',
+  h3 = 'NvimLightRed',
+  h4 = 'NvimLightGreen',
   h5 = 'NvimLightYellow',
 }
 
@@ -27,6 +27,8 @@ function M.set_normal_highlights()
   local heading_fg_values = { heading_colors.h1, heading_colors.h2, heading_colors.h3, heading_colors.h4, heading_colors.h5 }
   local heading_bg_values = { 'background', 'background', 'background', 'background', 'background' }
   vim.cmd(create_heading_highlight_command(heading_fg_values, heading_bg_values))
+  -- Checklists
+  vim.cmd 'hi @markup.list.checked.markdown guibg=NvimDarkGrey2 guifg=NvimLightCyan'
 end
 
 -- Set preview highlighting (inverted colors)
@@ -35,18 +37,22 @@ function M.set_preview_highlights()
   local heading_fg_values = { 'background', 'background', 'background', 'background', 'background' }
   local heading_bg_values = { heading_colors.h1, heading_colors.h2, heading_colors.h3, heading_colors.h4, heading_colors.h5 }
   vim.cmd(create_heading_highlight_command(heading_fg_values, heading_bg_values))
+  -- Checklists
+  vim.cmd 'hi @markup.list.checked.markdown guibg=NvimLightCyan guifg=NvimDarkGrey2'
 end
 
 -- Enable markdown preview mode
 function M.enable_preview()
   M.preview_enabled = true
   M.set_preview_highlights()
+  vim.opt.conceallevel = 2
 end
 
 -- Disable markdown preview mode
 function M.disable_preview()
   M.preview_enabled = false
   M.set_normal_highlights()
+  vim.opt.conceallevel = 0
 end
 
 -- Toggle markdown preview mode
