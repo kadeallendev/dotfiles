3b8e97fdfac27e2a5a9f584c8fbf1ecf6475b993
Kade Allen <38875538+kadeallendev@users.noreply.github.com>
2025-07-30 09:18:17 +1200

    Update highlights for xml and add html

---
+++ nvim/.config/nvim/after/ftplugin/html.lua
@@ -0,0 +1,7 @@
+-- Highlights
+vim.cmd [[
+  hi @tag.html guifg=NvimLightBlue
+  hi @tag.attribute.html guifg=NvimLightGrey4
+  hi @tag.delimiter.html guifg=NvimLightGrey4
+  hi @string.html guifg=NvimLightMagenta
+]]
--- nvim/.config/nvim/after/ftplugin/xml.lua
+++ nvim/.config/nvim/after/ftplugin/xml.lua
@@ -1,2 +1,10 @@
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
+
+-- Highlights
+vim.cmd [[
+  hi @tag.xml guifg=NvimLightBlue
+  hi @string.xml guifg=NvimLightMagenta
+  hi @tag.delimiter.xml guifg=NvimLightGrey4
+  hi @tag.attribute.xml guifg=NvimLightGrey4
+]]
