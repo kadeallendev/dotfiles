991b93a1dadbf8b5462bf10ade2d14bf5fa3740f
Kade Allen <38875538+kadeallendev@users.noreply.github.com>
2025-08-06 11:34:27 +1200

    Use default recipient if no KEYID

--- nvim/.config/nvim/plugin/encrypt.lua
+++ nvim/.config/nvim/plugin/encrypt.lua
@@ -2,7 +2,7 @@ local M = {}
 
 M.is_real_file = function()
   local filename = vim.api.nvim_buf_get_name(0)
-  return filename ~= "" and vim.fn.filereadable(filename) == 1
+  return filename ~= '' and vim.fn.filereadable(filename) == 1
 end
 
 --- Checks if the current buffer is GPG encrypted
@@ -10,7 +10,7 @@ end
 M.is_encrypted = function()
   local lines = vim.api.nvim_buf_get_lines(0, 0, 10, false)
   for _, line in ipairs(lines) do
-    if line:match("-----BEGIN PGP MESSAGE-----") then
+    if line:match '-----BEGIN PGP MESSAGE-----' then
       return true
     end
   end
@@ -28,16 +28,23 @@ end
 --- ```
 M.encrypt_buffer = function()
   if not M.is_real_file() then
-    vim.notify("Not a file on disk", vim.log.levels.WARN)
+    vim.notify('Not a file on disk', vim.log.levels.WARN)
     return
   elseif M.is_encrypted() then
-    vim.notify("Already Encrypted", vim.log.levels.INFO)
+    vim.notify('Already Encrypted', vim.log.levels.INFO)
     return
   end
 
-  vim.cmd "%! gpg --encrypt --armor --recipient $KEYID"
+  -- Get the recipient
+  local recipient = '--default-recipient-self'
+  local keyid = vim.env.KEYID
+  if keyid then
+    recipient = '--recipient ' .. keyid
+  end
+
+  vim.cmd(string.format('%%! gpg --encrypt --armor %s', recipient))
   vim.b.og_filetype = vim.bo.filetype
-  vim.bo.filetype = "gpg"
+  vim.bo.filetype = 'gpg'
   vim.cmd.write()
 end
 
@@ -51,24 +58,24 @@ end
 --- ```
 M.decrypt_buffer = function()
   if not M.is_real_file() then
-    vim.notify("Not a file on disk", vim.log.levels.WARN)
+    vim.notify('Not a file on disk', vim.log.levels.WARN)
     return
   elseif not M.is_encrypted() then
-    vim.notify("Not GPG encrypted", vim.log.levels.INFO)
+    vim.notify('Not GPG encrypted', vim.log.levels.INFO)
     return
   end
 
-  vim.cmd "%! gpg --quiet --decrypt"
+  vim.cmd '%! gpg --quiet --decrypt'
   vim.bo.filetype = vim.b.og_filetype
   vim.cmd.write()
 end
 
 -- Set keymaps
-vim.keymap.set('n', '<leader>e', M.encrypt_buffer, { desc = "GPG encrypt buffer" })
-vim.keymap.set('n', '<leader>E', M.decrypt_buffer, { desc = "GPG decrypt buffer" })
+vim.keymap.set('n', '<leader>e', M.encrypt_buffer, { desc = 'GPG encrypt buffer' })
+vim.keymap.set('n', '<leader>E', M.decrypt_buffer, { desc = 'GPG decrypt buffer' })
 
 -- Create commands
-vim.api.nvim_create_user_command('Enc', M.encrypt_buffer, { desc = "GPG encrypt buffer" })
-vim.api.nvim_create_user_command('Dec', M.decrypt_buffer, { desc = "GPG decrypt buffer" })
+vim.api.nvim_create_user_command('Enc', M.encrypt_buffer, { desc = 'GPG encrypt buffer' })
+vim.api.nvim_create_user_command('Dec', M.decrypt_buffer, { desc = 'GPG decrypt buffer' })
 
 return M
