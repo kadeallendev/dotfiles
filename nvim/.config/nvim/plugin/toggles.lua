e702008690f960e39642fbed1f04242c2054ecbb
Kade Allen <38875538+kadeallendev@users.noreply.github.com>
2025-08-06 11:00:58 +1200

    Update toggles.lua to use module

--- nvim/.config/nvim/plugin/toggles.lua
+++ nvim/.config/nvim/plugin/toggles.lua
@@ -1,44 +1,80 @@
--- Keymaps for toggling options
+--- Functions and keymaps for toggling options
+local M = {}
 
--- Wrapping
-vim.keymap.set('n', '<leader>twr', '<CMD>setlocal wrap! wrap?<CR>')
+------ WRAP ------
 
--- Cursor line
-vim.keymap.set('n', '<leader>tcl', '<CMD>setlocal cursorline! cursorline?<CR>', { desc = 'Toggle Cursor Line (Local)' })
+--- Toggles 'wrap' option
+M.toggle_wrapping = function()
+  vim.o.wrap = not vim.o.wrap
+  print(vim.o.wrap)
+end
 
--- Cursor column
-vim.keymap.set('n', '<leader>tcc', '<CMD>setlocal cursorcolumn! cursorcolumn?<CR>', { desc = 'Toggle Cursor Column (Local)' })
+vim.keymap.set('n', '<leader>twr', M.toggle_wrapping, { desc = 'Toggle wrap' })
 
--- Color column
-vim.keymap.set('n', '<leader>tCc', function()
+------ CURSOR LINE ------
+
+--- Toggles 'cursorline' option
+M.toggle_cursorline = function()
+  vim.o.cursorline = not vim.o.cursorline
+end
+
+vim.keymap.set('n', '<leader>tcl', M.toggle_cursorline, { desc = 'Toggle cursorline' })
+
+------ CURSOR COLUMN ------
+
+--- Toggles 'cursorcolumn' option
+M.toggle_cursorcolumn = function()
+  vim.o.cursorcolumn = not vim.o.cursorcolumn
+end
+
+vim.keymap.set('n', '<leader>tcc', M.toggle_cursorcolumn, { desc = 'Toggle Cursor Column (Local)' })
+
+------ COLOR COLUMN ------
+
+--- Toggles 'colorcolumn' option between '80' and ''
+M.toggle_colorcolumn = function()
   if vim.o.colorcolumn == '' then
     vim.o.colorcolumn = '80'
   else
     vim.o.colorcolumn = ''
   end
-end, { desc = 'Toggle color column' })
+end
 
--- Diagnostics
-vim.keymap.set('n', '<leader>td', function()
-  if vim.diagnostic.is_enabled() then
-    vim.diagnostic.enable(false)
-  else
-    vim.diagnostic.enable(true)
-  end
-end, { desc = 'Toggle diagnostics' })
+vim.keymap.set('n', '<leader>tCc', M.toggle_colorcolumn, { desc = 'Toggle color column' })
 
--- Spell
-vim.keymap.set('n', '<leader>ts', '<CMD>setlocal spell! spell?<CR>', { desc = 'Toggle Spell (Local)' })
 
--- Fold column
-vim.keymap.set('n', '<leader>tfc', function()
-  local cur_val = vim.wo.foldcolumn
-  if cur_val == '0' then
+------ DIAGNOSTICS ------
+
+--- Toggles 'vim.diagnostic' option
+M.toggle_diagnostic = function()
+  vim.diagnostic.enable(not vim.diagnostic.is_enabled())
+end
+
+vim.keymap.set('n', '<leader>td', M.toggle_diagnostic, { desc = 'Toggle diagnostics' })
+
+------ SPELL ------
+
+--- Toggles 'spell' option
+M.toggle_spell = function()
+  vim.o.spell = not vim.o.spell
+  print(vim.o.spell)
+end
+
+vim.keymap.set('n', '<leader>ts', M.toggle_spell, { desc = 'Toggle Spell (Local)' })
+
+------ FOLD COLUMN ------
+
+M.toggle_foldcolumn = function()
+  if vim.wo.foldcolumn == '0' then
     vim.wo.foldcolumn = 'auto'
   else
     vim.wo.foldcolumn = '0'
   end
-end, { desc = 'Toggle foldcolumn' })
+end
+-- Fold column
+vim.keymap.set('n', '<leader>tfc', M.toggle_foldcolumn, { desc = 'Toggle foldcolumn' })
 
--- Statusline
+------- STATUSLINE ------
 vim.keymap.set('n', '<leader>tS', require('kade.statusline-toggle').toggle, { desc = 'Toggle statusline' })
+
+return M
