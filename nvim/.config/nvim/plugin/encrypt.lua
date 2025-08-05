4135e42c24c1bb313a2ec2f5aa44161b8cfef6db
Kade Allen <38875538+kadeallendev@users.noreply.github.com>
2025-08-06 11:51:56 +1200

    Update doc comments for encrypt

--- nvim/.config/nvim/plugin/encrypt.lua
+++ nvim/.config/nvim/plugin/encrypt.lua
@@ -1,12 +1,14 @@
 local M = {}
 
+--- Checks if the current buffer is a real file on disk
+---@return boolean # |TRUE| if the buffer is a real file on disk, |FALSE| otherwise
 M.is_real_file = function()
   local filename = vim.api.nvim_buf_get_name(0)
   return filename ~= '' and vim.fn.filereadable(filename) == 1
 end
 
 --- Checks if the current buffer is GPG encrypted
----@return boolean True if GPG encrypted, false otherwise
+---@return boolean # |TRUE| if the buffer is GPG encrypted, |FALSE| otherwise
 M.is_encrypted = function()
   local lines = vim.api.nvim_buf_get_lines(0, 0, 10, false)
   for _, line in ipairs(lines) do
@@ -27,6 +29,7 @@ end
 --- gpg --encrypt --recipient $KEYID
 --- ```
 M.encrypt_buffer = function()
+  -- Ensure buffer is a file on disk and is not encrypted already
   if not M.is_real_file() then
     vim.notify('Not a file on disk', vim.log.levels.WARN)
     return
@@ -57,6 +60,7 @@ end
 --- gpg --quiet --decrypt
 --- ```
 M.decrypt_buffer = function()
+  -- Ensure buffer is a file on disk and is already encrypted
   if not M.is_real_file() then
     vim.notify('Not a file on disk', vim.log.levels.WARN)
     return
