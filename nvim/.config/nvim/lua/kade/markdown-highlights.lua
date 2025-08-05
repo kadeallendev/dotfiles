b3fe269d2a266852bd04b8f8b7b93440d8c62b18
Kade Allen <38875538+kadeallendev@users.noreply.github.com>
2025-08-06 11:01:09 +1200

    Update markdown-highlights to use vim.api

--- nvim/.config/nvim/lua/kade/markdown-highlights.lua
+++ nvim/.config/nvim/lua/kade/markdown-highlights.lua
@@ -12,13 +12,16 @@ local heading_colors = {
   h5 = 'NvimLightYellow',
 }
 
---- Generate highlight commands with foreground/background parameters
-local function create_heading_highlight_command(fg, bg)
-  local cmd = {}
-  for i = 1, 5 do
-    table.insert(cmd, string.format('hi @markup.heading.%d.markdown guifg=%s guibg=%s', i, fg[i], bg[i]))
+--- Sets the highlights for markdown headings
+--- Sets the @markup.heading.<level>.markdown group.
+---@param fg array Array of foreground colors
+---@param bg array Array of background colors
+local function set_heading_highlights(fg, bg)
+  local len = vim.tbl_count(fg)
+  for i = 1, len do
+    local group = string.format('@markup.heading.%d.markdown', i)
+    vim.api.nvim_set_hl(0, group, { fg = fg[i], bg = bg[i], bold = true })
   end
-  return table.concat(cmd, '\n')
 end
 
 --- Set normal highlighting (colored headings on default bg)
@@ -27,9 +30,9 @@ function M.set_normal_highlights()
   -- Headings
   local heading_fg_values = { heading_colors.h1, heading_colors.h2, heading_colors.h3, heading_colors.h4, heading_colors.h5 }
   local heading_bg_values = { 'background', 'background', 'background', 'background', 'background' }
-  vim.cmd(create_heading_highlight_command(heading_fg_values, heading_bg_values))
+  set_heading_highlights(heading_fg_values, heading_bg_values)
   -- Checklists
-  vim.cmd 'hi @markup.list.checked.markdown guibg=NvimDarkGrey2 guifg=NvimLightCyan'
+  vim.api.nvim_set_hl(0, '@markup.list.checked.markdown', { bg = 'NvimDarkGrey2', fg = 'NvimLightCyan' })
 end
 
 --- Set preview highlighting (inverted colors)
@@ -38,23 +41,27 @@ function M.set_preview_highlights()
   -- Headings
   local heading_fg_values = { 'background', 'background', 'background', 'background', 'background' }
   local heading_bg_values = { heading_colors.h1, heading_colors.h2, heading_colors.h3, heading_colors.h4, heading_colors.h5 }
-  vim.cmd(create_heading_highlight_command(heading_fg_values, heading_bg_values))
+  set_heading_highlights(heading_fg_values, heading_bg_values)
   -- Checklists
-  vim.cmd 'hi @markup.list.checked.markdown guibg=NvimLightCyan guifg=NvimDarkGrey2'
+  vim.api.nvim_set_hl(0, '@markup.list.checked.markdown', { bg = 'NvimLightCyan', fg = 'NvimDarkGrey2' })
 end
 
 --- Enable markdown preview mode
+--- Updates highlights, sets 'conceallevel' to 2, and disables diagnostics
 function M.enable_preview()
   M.preview_enabled = true
   M.set_preview_highlights()
   vim.opt.conceallevel = 2
+  vim.diagnostic.enable(false)
 end
 
 --- Disable markdown preview mode
+--- Updates highlights, sets 'conceallevel' to 0, and enables diagnostics
 function M.disable_preview()
   M.preview_enabled = false
   M.set_normal_highlights()
   vim.opt.conceallevel = 0
+  vim.diagnostic.enable(true)
 end
 
 --- Toggle markdown preview mode
