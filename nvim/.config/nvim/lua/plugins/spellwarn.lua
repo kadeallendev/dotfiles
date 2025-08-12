3ad7f3f55c1e59426132c831fb2202b20823e994
Kade Allen <38875538+kadeallendev@users.noreply.github.com>
2025-08-13 10:49:27 +1200

    Add spellwarn config to classify as hints and set global variable

--- nvim/.config/nvim/lua/plugins/spellwarn.lua
+++ nvim/.config/nvim/lua/plugins/spellwarn.lua
@@ -2,5 +2,17 @@
 return {
   'ravibrock/spellwarn.nvim',
   event = 'VeryLazy',
-  config = true,
+  config = function()
+    local spellwarn = require('spellwarn')
+    spellwarn.setup {
+      severity = {
+        spellbad = "HINT",
+        spellcap = "HINT",
+        spelllocal = "HINT",
+        spellrare = "HINT"
+      }
+    }
+    -- Set global variable
+    vim.g.spellwarn_enabled = true
+  end
 }
