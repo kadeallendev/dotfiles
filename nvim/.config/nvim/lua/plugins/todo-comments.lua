1188661702780ba5f4f0ad75c808c043b771430f
Kade Allen <38875538+kadeallendev@users.noreply.github.com>
2025-08-14 10:25:08 +1200

    Add keymaps for next and prev todos

--- nvim/.config/nvim/lua/plugins/todo-comments.lua
+++ nvim/.config/nvim/lua/plugins/todo-comments.lua
@@ -6,11 +6,17 @@ return {
     dependencies = {
       'nvim-lua/plenary.nvim',
     },
-    opts = {
-      signs = false,
-      highlight = {
-        multiline_pattern = '^|.',
-      },
-    },
+    config = function()
+      local todo = require('todo-comments')
+      todo.setup({
+        signs = false,
+        highlight = {
+          multiline_pattern = '^|.',
+        },
+      })
+
+      vim.keymap.set('n', ']t', todo.jump_next, { desc = "Next todo comment" })
+      vim.keymap.set('n', '[t', todo.jump_prev, { desc = "Previous todo comment" })
+    end
   },
 }
