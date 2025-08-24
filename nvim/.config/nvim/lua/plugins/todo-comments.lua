dc857339a268092909def6ed6ad72b704a09d329
Kade Allen <38875538+kadeallendev@users.noreply.github.com>
2025-08-25 08:47:59 +1200

    Todo comments highlight to yellow

--- nvim/.config/nvim/lua/plugins/todo-comments.lua
+++ nvim/.config/nvim/lua/plugins/todo-comments.lua
@@ -15,6 +15,10 @@ return {
         },
       })
 
+      -- Use yellow for TODO
+      vim.api.nvim_set_hl(0, 'TodoFGTODO', { fg = 'NvimLightYellow' })
+      vim.api.nvim_set_hl(0, 'TodoBGTODO', { bg = 'NvimLightYellow', fg = "NvimDarkGrey1" })
+
       vim.keymap.set('n', ']t', todo.jump_next, { desc = "Next todo comment" })
       vim.keymap.set('n', '[t', todo.jump_prev, { desc = "Previous todo comment" })
     end
