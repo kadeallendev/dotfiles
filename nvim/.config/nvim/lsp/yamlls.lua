be72e78f69d5074013268aa749e9dd7770cc57aa
Kade Allen <38875538+kadeallendev@users.noreply.github.com>
2025-08-28 09:18:21 +1200

    Add yamlls settings

---
+++ nvim/.config/nvim/lsp/yamlls.lua
@@ -0,0 +1,14 @@
+return {
+  cmd = { 'yaml-language-server', '--stdio' },
+  filetypes = { 'yaml', 'yaml.github-action', 'yaml.docker-compose', 'yaml.gitlab', 'yaml.helm-values' },
+  root_markers = { '.git' },
+  settings = {
+    yaml = {
+      schemas = {
+        ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
+        ["https://json.schemastore.org/github-action.json"] = "/.github/actions/*",
+      }
+    },
+    redhat = { telemetry = { enabled = false } }
+  }
+}
