3e2cbb451fbfceeb81b53e66587b6a3bc7dfd5c0
Kade Allen <38875538+kadeallendev@users.noreply.github.com>
2025-08-28 09:36:02 +1200

    Remove comment from cfg-treesitter

--- nvim/.config/nvim/lua/plugins/config/cfg-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/config/cfg-treesitter.lua
@@ -26,15 +26,3 @@ require('nvim-treesitter.configs').setup {
 }
 
 require('nvim-treesitter.install').compilers = { 'clang' }
-
--- local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
--- parser_config.powershell = {
---   install_info = {
---     url = vim.fn.stdpath 'data' .. '/tsparsers/tree-sitter-powershell',
---     files = { 'src/parser.c' },
---     branch = 'main',
---     generate_requires_npm = false,
---     requires_generate_from_grammar = false,
---   },
---   filetype = 'ps1',
--- }
