2eac38a9fb2f3d7e684be8e991eb2325749ff897
Kade Allen <38875538+kadeallendev@users.noreply.github.com>
2025-08-28 09:35:43 +1200

    Remove comment for ps1 ftplugin

--- nvim/.config/nvim/after/ftplugin/ps1.lua
+++ nvim/.config/nvim/after/ftplugin/ps1.lua
@@ -4,25 +4,3 @@ vim.opt.expandtab = true
 vim.highlight.priorities.semantic_tokens = 90
 
 vim.api.nvim_set_hl(0, '@variable.parameter.builtin.powershell', { fg = 'NvimLightMagenta' })
-
--- require('lspconfig').powershell_es.setup {
---   -- bundle_path = '~/.local/share/nvim/lsp/PowerShellEditorServices',
---   cmd = {
---     'pwsh',
---     '-NoLogo',
---     '-NoProfile',
---     '-Command',
---     '/home/kade/.local/share/nvim/lsp/PowerShellEditorServices/PowerShellEditorServices/Start-EditorServices.ps1',
---   },
---   on_attach = function(client, bufnr)
---     local defaults = require('plugins.lsp.keymaps').set_defaults
---     defaults(client, bufnr)
---   end,
---   settings = {
---     powershell = {
---       codeFormatting = {
---         Preset = 'OTBS',
---       },
---     },
---   },
--- }
