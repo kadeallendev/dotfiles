8d35ba609354b4215510ab7498c04971ad47ad75
Kade Allen <38875538+kadeallendev@users.noreply.github.com>
2025-09-10 08:54:45 +1200

    Remove colorschemes as they are slow

--- nvim/.config/nvim/lua/plugins/colorschemes.lua
+++ nvim/.config/nvim/lua/plugins/colorschemes.lua
@@ -1,59 +1,59 @@
 -- Colorschemes
 return {
   -- Rose Pine
-  {
-    'rose-pine/neovim',
-    name = 'rose-pine',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require('rose-pine').setup {
-        transparent = false,
-        variant = 'main',
-        enable = {
-          terminal = true,
-        },
-        styles = {
-          bold = true,
-          italic = false,
-          transparent = false,
-          transparency = false,
-        },
-      }
-    end,
-  },
-
-  -- Cyberdream
-  {
-    'scottmckendry/cyberdream.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require('cyberdream').setup {
-        transparent = true,
-        italic_comments = false,
-        terminal_colors = false,
-
-        -- Override default highlights
-        overrides = function(colors)
-          return {
-            LineNrAbove = { fg = colors.grey, bg = 'NONE' },
-            LineNr = { fg = colors.blue, bg = 'NONE' },
-            LineNrBelow = { fg = colors.grey, bg = 'NONE' },
-          }
-        end,
-        extensions = {
-          telescope = false,
-          fzflua = false,
-          gitsigns = true,
-          lazy = true,
-          cmp = true,
-        },
-      }
-    end,
-  },
-
-  {
-    'bettervim/yugen.nvim',
-  },
+  -- {
+  --   'rose-pine/neovim',
+  --   name = 'rose-pine',
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     require('rose-pine').setup {
+  --       transparent = false,
+  --       variant = 'main',
+  --       enable = {
+  --         terminal = true,
+  --       },
+  --       styles = {
+  --         bold = true,
+  --         italic = false,
+  --         transparent = false,
+  --         transparency = false,
+  --       },
+  --     }
+  --   end,
+  -- },
+  --
+  -- -- Cyberdream
+  -- {
+  --   'scottmckendry/cyberdream.nvim',
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     require('cyberdream').setup {
+  --       transparent = true,
+  --       italic_comments = false,
+  --       terminal_colors = false,
+  --
+  --       -- Override default highlights
+  --       overrides = function(colors)
+  --         return {
+  --           LineNrAbove = { fg = colors.grey, bg = 'NONE' },
+  --           LineNr = { fg = colors.blue, bg = 'NONE' },
+  --           LineNrBelow = { fg = colors.grey, bg = 'NONE' },
+  --         }
+  --       end,
+  --       extensions = {
+  --         telescope = false,
+  --         fzflua = false,
+  --         gitsigns = true,
+  --         lazy = true,
+  --         cmp = true,
+  --       },
+  --     }
+  --   end,
+  -- },
+  --
+  -- {
+  --   'bettervim/yugen.nvim',
+  -- },
 }
