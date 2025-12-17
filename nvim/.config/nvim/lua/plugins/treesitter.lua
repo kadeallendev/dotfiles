052df7443e7b91243d7f555001ad6f488441c5f2
Kade Allen <38875538+kadeallendev@users.noreply.github.com>
2025-12-17 13:54:47 +1300

    Specify branches of treesitter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -2,9 +2,10 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   dependencies = {
-    'nvim-treesitter/nvim-treesitter-textobjects',
+    { 'nvim-treesitter/nvim-treesitter-textobjects', branch = "main" },
     -- 'nvim-treesitter/nvim-treesitter-context',
   },
+  branch = "master",
   -- tag = "v0.9.3",
   build = ':TSUpdate',
   lazy = false,
