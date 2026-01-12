b9f9b854b312293f943706ab0140f9a78991bec6
Kade Allen <38875538+kadeallendev@users.noreply.github.com>
2026-01-13 11:16:05 +1300

    del nabla

--- nvim/.config/nvim/lua/plugins/inactive/nabla.lua
+++
@@ -1,9 +0,0 @@
--- LaTeX equation viewer
-return {
-  -- 'jbyuki/nabla.nvim',
-  -- event = 'VeryLazy',
-  -- config = function()
-  --   local nabla = require 'nabla'
-  --   vim.keymap.set('n', '<leader>p', nabla.popup, { desc = 'Display latex equation' })
-  -- end,
-}
